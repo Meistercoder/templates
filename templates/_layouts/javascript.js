3b44a37073379e7a662d72c1439a65cf615302d9
Unknown <fe.fessler@gmx.de>
2018-07-28 14:27:25 +0200

    Erste Kategoriesierungsversuch

--- templates/_layouts/javascript.js
+++ templates/_layouts/javascript.js
@@ -12,6 +12,7 @@ $(window).scroll ( function() {
  * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
  * Copyright 2017.
  * Licensed under the GPL license
+ The HTMLElement.offsetParent returns a reference to the object which is the closest (nearest in the containment hierarchy) positioned containing element.
  */
 
 
