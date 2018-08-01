0924995aed1f5625f7da213f2511eaf0f92b402a
Unknown <fe.fessler@gmx.de>
2018-08-01 14:06:27 +0200

    sichern der website

---
+++ templates/craft_sicherungWebsite/web/CSSundJS/javascript.js
@@ -0,0 +1,41 @@
+$(window).scroll ( function() {
+    if ($(document).scrollTop() > 50) {
+        document.getElementById('rechteck').style.height = '50px';
+        document.getElementById('collapsibleNavbar').style.marginTop = '-55px'; //For eg
+
+    }
+}
+);
+
+
+/*!
+ * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
+ * Copyright 2017.
+ * Licensed under the GPL license
+ The HTMLElement.offsetParent returns a reference to the object which is the closest (nearest in the containment hierarchy) positioned containing element.
+ */
+
+
+$( document ).ready( function () {
+    $( '.dropdown-menu a.dropdown-toggle' ).on( 'click', function ( e ) {
+        var $el = $( this );
+        var $parent = $( this ).offsetParent( ".dropdown-menu" );
+        if ( !$( this ).next().hasClass( 'show' ) ) {
+            $( this ).parents( '.dropdown-menu' ).first().find( '.show' ).removeClass( "show" );
+        }
+        var $subMenu = $( this ).next( ".dropdown-menu" );
+        $subMenu.toggleClass( 'show' );
+
+        $( this ).parent( "li" ).toggleClass( 'show' );
+
+        $( this ).parents( 'li.nav-item.dropdown.show' ).on( 'hidden.bs.dropdown', function ( e ) {
+            $( '.dropdown-menu .show' ).removeClass( "show" );
+        } );
+
+         if ( !$parent.parent().hasClass( 'navbar-nav' ) ) {
+            $el.next().css( { "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 } );
+        }
+
+        return false;
+    } );
+} );
