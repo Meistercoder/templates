6d4dfcb70a70866c7fffd5a75b78985b5154d678
Unknown <fe.fessler@gmx.de>
2018-08-10 18:31:45 +0200

    navbar wieder weng verbessert

--- templates/craft_sicherungWebsite/web/CSSundJS/javascript.js
+++ templates/craft_sicherungWebsite/web/CSSundJS/javascript.js
@@ -5,4 +5,25 @@ $(window).scroll ( function() {
 
     }
 }
+
 );
+
+/* Funktion, um Links klickbar zu machen auf großen Bildschirm, jedoch nicht auf kleinen */
+
+$( document ).ready(function() {
+if ($(window).width() < 992) {
+  $(".nav-link").attr("data-toggle", "dropdown");
+}
+else {
+  // Get all nav-links inside the navbar
+  var navlinks = document.getElementsByClassName("nav-link");
+
+  // Loop through the navbars
+  for (var i = 0; i < navlinks.length ; i++) {
+    console.log("data toogle geändert");
+    navlinks[i].outerHTML = navlinks[i].outerHTML.replace("data-toggle=\"dropdown\"","data-toggle=\"dropdown disabled\"");
+  }
+  $(".nav-link").attr("data-toggle", "dropdown disabled");
+  console.log("data toogle geändert");
+}
+});
