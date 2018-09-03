cf294fabc0ec2ffef38bd6c38f7aa0e7d85b11ce
Unknown <fe.fessler@gmx.de>
2018-09-04 00:04:09 +0200

    Bootstrap nur in Teilen der Seite aktivieren, Navbar fertig machen

--- templates/craft_sicherungWebsite/web/CSSundJS/javascript.js
+++ templates/craft_sicherungWebsite/web/CSSundJS/javascript.js
@@ -1,7 +1,7 @@
 $(window).scroll ( function() {
     if ($(document).scrollTop() > 50) {
         document.getElementById('rechteck').style.height = '50px';
-        document.getElementById('collapsibleNavbar').style.marginTop = '-55px'; //For eg
+        document.getElementById('collapsibleNavbar').style.marginTop = '-58px'; //For eg
 /* document.getElementById ist eine JavaScript-Funktion. Überall, wo das $-Zeichen
 davor steht, wird JQuery verwendet. Es macht genau das Selbe, ist nur nicht so
 verbose. Einfach kürzere Syntax, sonst kein Unterschied. */
@@ -54,8 +54,8 @@ function navlinksAnpassen () {
         $("#verschwinde-lg-2").attr("class", "col-lg-2");
         $("#verschwinde-gros").attr("class", " ");
 
-        $("#footerHandy").css("display", "none")
-        $("#footerDesktop").css("display", "block") /*Der mobile oder standard-footer wird angezeigt, je nach bildschirmgröße */
+        $("#footerHandy").css("display", "none");
+        $("#footerDesktop").css("display", "block"); /*Der mobile oder standard-footer wird angezeigt, je nach bildschirmgröße */
   }
 }
 
@@ -66,14 +66,25 @@ function WelleAnpassen () {
     $("#welle").css("width", "100%"); /*Die Welle größer machen auf kleinen Bildschirmen */
     $("#welle").css("margin-left", "-50%"); /*Die Welle größer machen auf kleinen Bildschirmen */
 
-    $("#empfehlungenMobil").css("display", "block")
-    $("#empfehlungenDesktop").css("display", "none") /*Der mobile oder standard-footer wird angezeigt, je nach bildschirmgröße */
+    $("#empfehlungenMobil").css("display", "block");
+    $("#empfehlungenDesktop").css("display", "none"); /*Die mobilen oder standard-empfehlungen werden angezeigt, je nach bildschirmgröße */
   }
   else {
       $("#welle").css("width", "40%"); /*Die Welle größer machen auf kleinen Bildschirmen */
       $("#welle").css("margin-left", "0"); /*Die Welle größer machen auf kleinen Bildschirmen */
 
-      $("#empfehlungenDesktop").css("display", "block")
-      $("#empfehlungenMobil").css("display", "none") /*Der mobile oder standard-footer wird angezeigt, je nach bildschirmgröße */
+      $("#empfehlungenDesktop").css("display", "block");
+      $("#empfehlungenMobil").css("display", "none"); /*Der mobile oder standard-footer wird angezeigt, je nach bildschirmgröße */
+  }
+
+  if ($(window).innerWidth() < 1000 ) { //Die Navbar anpassen
+
+  $("#navbarDesktop").css("display", "none");
+  $("#navbarHandy").css("display", "block");
+
+  }
+  else {
+    $("#navbarDesktop").css("display", "block");
+    $("#navbarHandy").css("display", "none");
   }
 }
